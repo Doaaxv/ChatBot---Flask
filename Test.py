0a36b3f4797a2cf67483671acb5a10d0078dbab8
Kahlid <kha8lid@hotmail.com>
2020-04-25 08:02:20 +0300

    chat loading response

--- Test.py
+++ Test.py
@@ -6,6 +6,7 @@ import warnings
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import re
+from flask import Flask, render_template, request
 
 warnings.filterwarnings('ignore')
 
@@ -14,69 +15,85 @@ warnings.filterwarnings('ignore')
 
 f = open("info.txt","r", errors='ignore')
 text = f.read()
-#text = text.lower()
-#text = text.strip('\n\t[]<>/</br>See COVID-19')
-#text = text.replace('.,','')
-#text = text.replace('Additional information on how COVID-19 is spread is available at How COVID-19 Spreads Related: Talking to Children','')
-#text = text.replace(':,','')
-#text = text.replace(' , ','')
-#text = text.replace('[','')
-#text = text.replace(']','')
-#text = re.sub('<[^<]+?>', '', text)
 
-sent_tokens = nltk.sent_tokenize(text)
-word_tokens = nltk.word_tokenize(text)
+def chatbot(user_response):
 
-#print(str(word_tokens) +' -----'+ str(sent_tokens))
+    sent_tokens = nltk.sent_tokenize(text)
+    word_tokens = nltk.word_tokenize(text)
 
-lemmer = nltk.WordNetLemmatizer()
+    lemmer = nltk.WordNetLemmatizer()
 
-def LemTokens(tokens):
-    return [lemmer.lemmatize(token) for token in tokens]
+    def LemTokens(tokens):
+        return [lemmer.lemmatize(token) for token in tokens]
 
-remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+    remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
 
-def LemNormalize(txt):
-    return LemTokens(nltk.word_tokenize(txt.lower().translate(remove_punct_dict)))
+    def LemNormalize(txt):
+        return LemTokens(nltk.word_tokenize(txt.lower().translate(remove_punct_dict)))
 
-def response(user_response):
-    chatbot_response = ''
-    sent_tokens.append(user_response)
-    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
-    tfidf = TfidfVec.fit_transform(sent_tokens)
-    vals = cosine_similarity(tfidf[-1],tfidf)
-    idx = vals.argsort()[0][-2]
-    flat = vals.flatten()
-    flat.sort()
-    req = flat[-2]
-    if(req==0):
-        chatbot_response = chatbot_response + 'Sorry, cannot understand your query'
-        return chatbot_response
-    else:
-        chatbot_response = chatbot_response+sent_tokens[idx]
-        #print(sent_tokens)
-        return chatbot_response
+    def response(user_response):
+        chatbot_response = ''
+        sent_tokens.append(user_response)
+        TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+        tfidf = TfidfVec.fit_transform(sent_tokens)
+        vals = cosine_similarity(tfidf[-1],tfidf)
+        idx = vals.argsort()[0][-2]
+        flat = vals.flatten()
+        flat.sort()
+        req = flat[-2]
+        if(req==0):
+            chatbot_response = chatbot_response + 'Sorry, cannot understand your query'
+            return chatbot_response
+        else:
+            chatbot_response = chatbot_response+sent_tokens[idx]
+            return chatbot_response
 
-flag = True
-important_words = ['vaccine', 'old people', 'cat', 'pet', 'animal', 'zoo', 'sun', 'weather', 'surface'
-                 'cloth', 'sneeze', 'sneezing','coughing','cough', 'distance', 'sick'
-                ,'illness', 'ill', 'clean','protect', 'symptoms', 'spread',]
-while(flag):
-    user_response = input()
-    user_response = user_response.lower()
-    #user_response = user_response.replace('virus' , '')
-    #user_response = user_response.replace('covid-19' , '')
-    for word in range(len(important_words)):
-        #print('----------------'+important_words[word]+''+str(user_response.find(important_words[word])))
-        if  1==0:#important_words[word] in user_response:
-            user_response = important_words[word]
-            break
-    #user_response = user_response.lower()
+    flag = True
+#    f_out= open("info.txt", 'r', errors='ignore')
+#    txtfile = f_out.read()
 
-    if(user_response!= "bye"):
-        print('chatbot: ', end='' )
-        print(response(user_response))
-        sent_tokens.remove(user_response)
-    else:
-        flag = False
-        print('bye')
+    #pointless while loop, should be removed
+    while(flag):
+
+        user_response = user_response.lower()
+        if(user_response!= "bye"):
+            bot_response = response(user_response)
+            sent_tokens.remove(user_response)
+            return(bot_response)
+        else:
+            flag = False
+            return 'bye'
+
+############################################## WEB PAGE #################################################################
+app = Flask(__name__)
+
+#@app.route('/home')
+#def index():
+#    return render_template('index.html')
+def word_count(string):
+    tokens = string.split()
+    n_tokens = len(tokens)
+    return n_tokens 
+
+@app.route('/')
+def index():
+    return render_template('firstpage.html')
+
+@app.route('/contact' , methods=['GET','POST'])
+def contact():
+    return render_template('contact.html')
+
+@app.route('/home' , methods=['GET'])
+def home():
+    return render_template('firstpage.html')
+
+@app.route('/process')# methods=['POST'])
+def process():
+    user_input = request.args.get("user_input")#.form['user_input']
+    if word_count(user_input) < 3:
+        return ("Sorry, in order to get the best response possible, your query must be at least three words long.")
+    output = chatbot(user_input)
+    return output#render_template('index.html', user_input = user_input, bot_response=output)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8081)
